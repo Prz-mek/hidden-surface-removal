d00ef39ad26d293f48d573eda79c1a2df5f5c3a7
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-05-21 17:08:08 +0200

    Added checks for edge conditions

--- src/main/java/com/example/hsr/model/transformations/Polygon.java
+++ src/main/java/com/example/hsr/model/transformations/Polygon.java
@@ -9,49 +9,48 @@ public class Polygon implements Comparable<Polygon> {
     private Point[] vertices;
 
     public Polygon(Point a, Point b, Point c, Point d) {
-        this.vertices = new Point[] {a, b, c, d};
+        this.vertices = new Point[] { a, b, c, d };
     }
 
     @Override
     public int compareTo(Polygon p) {
-        RealMatrix plane =  new Array2DRowRealMatrix(new double[][] {
+        RealMatrix plane = new Array2DRowRealMatrix(new double[][] {
                 { -p.vertices[0].getX(), -p.vertices[0].getY(), -p.vertices[0].getZ() },
                 { -p.vertices[1].getX(), -p.vertices[1].getY(), -p.vertices[1].getZ() },
                 { -p.vertices[2].getX(), -p.vertices[2].getY(), -p.vertices[2].getZ() }
         });
 
         double detCamera = new LUDecomposition(plane).getDeterminant();
-        double maxDet = 0.0;
+        double prevDet = 0.0;
 
         for (Point v : vertices) {
-            RealMatrix help =  new Array2DRowRealMatrix(new double[][] {
+            RealMatrix help = new Array2DRowRealMatrix(new double[][] {
                     { v.getX(), v.getY(), v.getZ() },
                     { v.getX(), v.getY(), v.getZ() },
                     { v.getX(), v.getY(), v.getZ() }
             });
             double detPolygon = new LUDecomposition(plane.add(help)).getDeterminant();
-            if (Math.abs(detPolygon) > Math.abs(maxDet)) {
-                maxDet = detPolygon;
+            if (Math.round(detPolygon) != 0) {
+                if (prevDet * detPolygon < 0) {
+                    return 0;
+                }
+                prevDet = detPolygon;
             }
         }
 
-        if (maxDet * detCamera < 0) {
-            return -1;
-        } else {
-            return 1;
-        }
+        return prevDet * detCamera < 0 ? -1 : 1;
     }
 
     public double[] getX() {
-        return new double[]{vertices[0].getX(), vertices[1].getX(), vertices[2].getX(), vertices[3].getX()};
+        return new double[] { vertices[0].getX(), vertices[1].getX(), vertices[2].getX(), vertices[3].getX() };
     }
 
     public double[] getY() {
-        return new double[]{vertices[0].getY(), vertices[1].getY(), vertices[2].getY(), vertices[3].getY()};
+        return new double[] { vertices[0].getY(), vertices[1].getY(), vertices[2].getY(), vertices[3].getY() };
     }
 
     public double[] getZ() {
-        return new double[]{vertices[0].getZ(), vertices[1].getZ(), vertices[2].getZ(), vertices[3].getZ()};
+        return new double[] { vertices[0].getZ(), vertices[1].getZ(), vertices[2].getZ(), vertices[3].getZ() };
     }
 
     public void transform(RealMatrix matrix) {
