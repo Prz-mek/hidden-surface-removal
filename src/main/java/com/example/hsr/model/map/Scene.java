3a05ece4cc43075846a2d16b1a702040f6cc6fe7
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-05-21 17:05:19 +0200

    Ensured all coordinates are different

--- src/main/java/com/example/hsr/model/map/Scene.java
+++ src/main/java/com/example/hsr/model/map/Scene.java
@@ -4,9 +4,9 @@ import javafx.scene.paint.Color;
 
 public class Scene {
     public static final Cuboid[] DEFAULT_VIEW = new Cuboid[] {
-            new Cuboid(500, 600, 3400, 900, -400, 3800, Color.WHEAT),
-            new Cuboid(-1000, 600, 3100, -300, -500, 3800, Color.RED),
-            new Cuboid(500, 600, 1900, 1300, -100, 2300, Color.BLUE),
-            new Cuboid(-800, 600, 1900, -300, -600, 3000, Color.GREEN)
+            new Cuboid(500, 600, 3400, 900, -400, 3700, Color.WHEAT),
+            new Cuboid(-1000, 650, 3100, -300, -500, 3800, Color.RED),
+            new Cuboid(550, 550, 1900, 1300, -100, 2300, Color.BLUE),
+            new Cuboid(-800, 700, 1800, -300, -600, 2700, Color.GREEN)
     };
 }
